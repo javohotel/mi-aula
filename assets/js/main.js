d804c59354fc29f611799ce4530036182330f2ab
Javier Ramirez <javirramirez@falabella.cl>
2024-07-12 13:06:11 -0400

    collapse menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-console.log("hola");
+$(document).ready(function() {
+    
+    $('#colpaseMenu').on('click', function() {
+        $('#menuPrincipal').toggle();
+        $('#mainSidebar').toggleClass('col-md-3 col-lg-3').toggleClass('col-md-1 col-lg-1');
+        $('#main').toggleClass('col-md-9 ms-sm-auto col-lg-9').toggleClass('col-md-11 ms-sm-auto col-lg-11');
+        $('.colapseText').toggle();
+        $('#colapseIcon').toggleClass('bi-arrow-bar-left').toggleClass('bi-arrow-bar-right');
+    })
+
+    
+})
