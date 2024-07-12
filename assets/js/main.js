70de74fda28aacca86137c96f3d88bd9c9ceb44d
Javier Ramirez <javirramirez@falabella.cl>
2024-07-12 16:46:50 -0400

    add menu collapse

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
     
     $('#colpaseMenu').on('click', function() {
-        $('#menuPrincipal').toggle();
-        $('#mainSidebar').toggleClass('col-md-3 col-lg-3').toggleClass('col-md-1 col-lg-1');
-        $('#main').toggleClass('col-md-9 ms-sm-auto col-lg-9').toggleClass('col-md-11 ms-sm-auto col-lg-11');
-        $('.colapseText').toggle();
+        colapseMenu();
+    });
+
+    function colapseMenu() {
+        $('.hideMenu').toggleClass('menu-hide');
+        $('#mainSidebar').toggleClass('colapsado');
+        $('#main').toggleClass('colapsado');
+        $('#colapseText').toggle();
         $('#colapseIcon').toggleClass('bi-arrow-bar-left').toggleClass('bi-arrow-bar-right');
-    })
+    }
 
     
 })
