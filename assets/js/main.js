067d6a3b97e34faa50da54e8043a390c6fe29e6c
Javier Ramirez <javirramirez@falabella.cl>
2024-07-20 14:18:04 -0400

    add interior contenidos page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+
+    //Navegacion interna
+    $('#navegacion-interna').on('click', 'a', function(e) {
+        e.preventDefault();
+        $('iframe').attr('src', $(this).attr('href'));
+    } )
+
+    //Abrir el menu colapsado solo en desktop
+    $(document).ready(function(){
+        var windowWidth = $(window).width();
+        if(windowWidth >= 1024) {
+           $('#menuAccordion button').click()
+        }
+      });
     
     $('#colpaseMenu').on('click', function() {
         colapseMenu();
