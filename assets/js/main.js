eb6d06b03dc88cfeabc0d03ff0dbabfc9e5c7389
Javier Ramirez <javirramirez@falabella.cl>
2024-11-15 10:53:44 -0300

    add calendar

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,5 +32,50 @@ $(document).ready(function() {
         $('#colapseIcon').toggleClass('bi-arrow-bar-left').toggleClass('bi-arrow-bar-right');
     }
 
-    
+
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list' ],
+      locale:'es',
+      firstDay:1,
+      buttonText:{
+        today:    'Hoy',
+        month:    'Mes',
+        week:     'Semana',
+        day:      'Día',
+        list:     'Lista'
+      },
+      height: 'parent',
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,listWeek'
+      },
+      defaultView: 'dayGridMonth',
+      defaultDate: '2024-11-11',
+      navLinks: true, // can click day/week names to navigate views
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events,
+      allDayText: 'Todo el día',
+      noEventsMessage: 'Sin eventos',
+      events: [
+        {
+          title: 'Todo el dia',
+          start: '2024-11-12',
+        },
+        {
+          title: 'Long Event',
+          start: '2024-11-09',
+          end: '2024-11-11'
+        },
+        {
+          groupId: 999,
+          title: 'Repeating Event',
+          start: '2024-11-27T16:00:00'
+        },
+      ]
+    });
+
+    calendar.render();
 })
